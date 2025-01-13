5f361fe89f191489f959fe32f71d62e17eda8c4d
Alexei_Lebedko <streiker.saik@gmail.com>
2025-01-13 16:21:38 +0300

    переписана с учетом нового модуля widget.py

--- main.py
+++ main.py
@@ -1,29 +1,45 @@
-from src.masks import get_mask_account, get_mask_card_number
+# from src.masks import get_mask_account, get_mask_card_number
+from src.widget import get_date, mask_account_card
 
-while True:
-    try:
-        card_number = int(input("Ввведите номер карты: "))
-    except ValueError:
-        print("ОШИБКА: Номер карты должен состоять из цифр, попробуйту еще")
-    else:
-        if len(str(card_number)) == 16:
-            break
-        else:
-            print("Номер карты состоит из 16 цифр")
+number_list = [
+    "Maestro 1596837868705199",
+    "Счет 64686473678894779589",
+    "MasterCard 7158300734726758",
+    "Счет 35383033474447895560",
+    "Visa Classic 6831982476737658",
+    "Visa Platinum 8990922113665229",
+    "Visa Gold 5999414228426353",
+    "Счет 73654108430135874305",
+]
+data = "2024-03-11T02:26:18.671407"
 
-while True:
-    try:
-        account_number = int(input("Ввведите номер счета: "))
-    except ValueError:
-        print("ОШИБКА: Номер счета состоять из цифр, попробуйту еще")
-    else:
-        if len(str(account_number)) == 20:
-            break
-        else:
-            print("Номер счета состоит из 20 цифр")
+print(mask_account_card(number_list[0]))
+print(get_date(data))
 
-mask_card_number = get_mask_card_number(card_number)
-mask_account = get_mask_account(account_number)
+# while True:
+#     try:
+#         card_number = int(input("Ввведите номер карты: "))
+#     except ValueError:
+#         print("ОШИБКА: Номер карты должен состоять из цифр, попробуйту еще")
+#     else:
+#         if len(str(card_number)) == 16:
+#             break
+#         else:
+#             print("Номер карты состоит из 16 цифр")
+#
+# while True:
+#     try:
+#         account_number = int(input("Ввведите номер счета: "))
+#     except ValueError:
+#         print("ОШИБКА: Номер счета состоять из цифр, попробуйту еще")
+#     else:
+#         if len(str(account_number)) == 20:
+#             break
+#         else:
+#             print("Номер счета состоит из 20 цифр")
 
-print(mask_card_number)
-print(mask_account)
+# mask_card_number = get_mask_card_number(card_number)
+# mask_account = get_mask_account(account_number)
+
+# print(mask_card_number)
+# print(mask_account)
