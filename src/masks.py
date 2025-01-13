8d28a89ff3dde595390bd7a79b1069d0fcadd3d4
Alexei_Lebedko <streiker.saik@gmail.com>
2025-01-13 16:20:10 +0300

    срезы делаются с конца строки и изменена аннотация типов

--- src/masks.py
+++ src/masks.py
@@ -1,10 +1,12 @@
-def get_mask_card_number(card_number: int) -> str:
+from typing import Union
+
+def get_mask_card_number(card_number: Union[int, str]) -> str:
     """Функция переводит целое число в строку и записывает через f строку со срезом: XXXX XX** **** XXXX"""
     card_number_string = str(card_number)
-    return f"{card_number_string[:4]} {card_number_string[4:6]}** **** {card_number_string[-4:]}"
+    return f"{card_number_string[-16:-12]} {card_number_string[-12:-10]}** **** {card_number_string[-4:]}"
 
 
-def get_mask_account(account_number: int) -> str:
+def get_mask_account(account_number: Union[int, str]) -> str:
     """Функция переводит целое число в строку и записывает через f строку со срезом: **XXXX"""
     account_number_string = str(account_number)
     return f"**{account_number_string[-4:]}"
