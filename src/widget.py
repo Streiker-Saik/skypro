c795e4720e8f92f6b395142a82dc1e24e893b8e7
Alexei_Lebedko <streiker.saik@gmail.com>
2025-01-13 16:30:18 +0300

    index_spice переименован в index_space

--- src/widget.py
+++ src/widget.py
@@ -4,13 +4,13 @@ from src.masks import get_mask_account, get_mask_card_number
 def mask_account_card(number_card_account: str) -> str:
     """Функция принимает "название карты" "номер" или "счет" и выводит через f строку название и маску"""
     if number_card_account.count(" ") == 1:
-        index_spice = number_card_account.index(" ")
+        index_space = number_card_account.index(" ")
     else:
-        index_spice = number_card_account.find(" ", number_card_account.find(" ") + 1)
+        index_space = number_card_account.find(" ", number_card_account.find(" ") + 1)
     return (
-        f"{number_card_account[:index_spice]} {get_mask_account(number_card_account)}"
-        if number_card_account[:index_spice] == "Счет"
-        else f"{number_card_account[:index_spice]} {get_mask_card_number(number_card_account)}"
+        f"{number_card_account[:index_space]} {get_mask_account(number_card_account)}"
+        if number_card_account[:index_space] == "Счет"
+        else f"{number_card_account[:index_space]} {get_mask_card_number(number_card_account)}"
     )
 
 
