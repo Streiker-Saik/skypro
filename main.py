ed00625cbad26f99010e51a3808fd68226276040
Alexei <streiker.saik@gmail.com>
2025-01-20 13:52:41 +0300

    main.py добалены функции: filter_by_state, sort_by_date

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # from src.masks import get_mask_account, get_mask_card_number
+from src.processing import filter_by_state, sort_by_date
 from src.widget import get_date, mask_account_card
 
 card_account_list = [
@@ -13,8 +14,19 @@ card_account_list = [
 ]
 date = "2024-03-11T02:26:18.671407"
 
+user_id_list = [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+]
+
 print(mask_account_card(card_account_list[0]))
 print(get_date(date))
+
+print(filter_by_state(user_id_list))
+print(filter_by_state(user_id_list, "CANCELED"))
+print(sort_by_date(user_id_list))
 
 # while True:
 #     try:
