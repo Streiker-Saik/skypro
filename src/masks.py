204365eda7e6653f74e8930af8d184db28d9056f
Alexei <streiker.saik@gmail.com>
2025-01-29 09:19:15 +0300

    улучшенна masks и написан test_masks

--- src/masks.py
+++ src/masks.py
@@ -1,13 +1,32 @@
-from typing import Union
-
-
-def get_mask_card_number(card_number: Union[int, str]) -> str:
+def get_mask_card_number(card_number: int) -> str:
     """Функция переводит целое число в строку и записывает через f строку со срезом: XXXX XX** **** XXXX"""
+
+    if card_number is None:
+        raise TypeError("Вводные дынные отсутствуют")
+
+    if not isinstance(card_number, int):
+        raise TypeError("Введено не числовое значение")
+
     card_number_string = str(card_number)
+
+    if len(card_number_string) != 16 or not card_number_string.isdigit():
+        raise ValueError("В номере карты должно быть 16 цифр")
+
     return f"{card_number_string[-16:-12]} {card_number_string[-12:-10]}** **** {card_number_string[-4:]}"
 
 
-def get_mask_account(account_number: Union[int, str]) -> str:
+def get_mask_account(account_number: int) -> str:
     """Функция переводит целое число в строку и записывает через f строку со срезом: **XXXX"""
+
+    if account_number is None:
+        raise TypeError("Вводные дынные отсутствуют")
+
+    if not isinstance(account_number, int):
+        raise TypeError("Введено не числовое значение")
+
     account_number_string = str(account_number)
+
+    if len(account_number_string) != 20 or not account_number_string.isdigit():
+        raise ValueError("В номере счета должно быть 20 цифр")
+
     return f"**{account_number_string[-4:]}"
---
+++ tests/test_masks.py
@@ -0,0 +1,48 @@
+import pytest
+
+from src.masks import get_mask_account, get_mask_card_number
+
+
+def test_get_mask_card_number(card_number):
+    """
+    Тестирование правильности маскирования номера карты
+    Проверка работы функции на различных входных форматах номеров карт,
+    включая граничные случаи и нестандартные длины номеров.
+    Проверка, что функция корректно обрабатывает входные строки, где отсутствует номер карты
+    """
+
+    with pytest.raises(TypeError) as exc_info:
+        get_mask_card_number(None)
+    assert str(exc_info.value) == "Вводные дынные отсутствуют"
+
+    with pytest.raises(TypeError) as exc_info:
+        get_mask_card_number("")
+    assert str(exc_info.value) == "Введено не числовое значение"
+
+    with pytest.raises(ValueError) as exc_info:
+        get_mask_card_number(1)
+    assert str(exc_info.value) == "В номере карты должно быть 16 цифр"
+
+    assert get_mask_card_number(card_number) == "7000 79** **** 6361"
+
+
+def test_get_mask_account(account_number):
+    """
+    Тестирование правильности маскирования номера счета.
+    Проверка работы функции с различными форматами и длинами номеров счетов.
+    Проверка, что функция корректно обрабатывает входные данные, где номер счета меньше ожидаемой длины.
+    """
+
+    with pytest.raises(TypeError) as exc_info:
+        get_mask_account(None)
+    assert str(exc_info.value) == "Вводные дынные отсутствуют"
+
+    with pytest.raises(TypeError) as exc_info:
+        get_mask_account("")
+    assert str(exc_info.value) == "Введено не числовое значение"
+
+    with pytest.raises(ValueError) as exc_info:
+        get_mask_account(1)
+    assert str(exc_info.value) == "В номере счета должно быть 20 цифр"
+
+    assert get_mask_account(account_number) == "**4305"
