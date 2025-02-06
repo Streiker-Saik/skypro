aa795939e21070cf03b3c8f6e2c39f5164cfbbc3
Alexei <streiker.saik@gmail.com>
2025-02-06 14:12:44 +0300

    add card_number_generator and test_card_number_generator

--- src/generators.py
+++ src/generators.py
@@ -1,11 +1,9 @@
-from typing import List, Dict, Iterator
+from typing import Dict, Iterator, List, Union
 
 
 def filter_by_currency(transactions: List[Dict], currency: str) -> Iterator[Dict]:
-    """
-    Функция получает список словарей транзакций и наименование валюты,
-    возвращает итератор транзакций отфильтрованный по наименованию валюты
-    """
+    """Функция получает список словарей транзакций и наименование валюты,
+    возвращает итератор транзакций отфильтрованный по наименованию валюты"""
     return filter(
         lambda code_currency: code_currency.get("operationAmount", 0).get("currency", 0).get("code", 0) == currency,
         transactions,
@@ -17,7 +15,22 @@ def transaction_descriptions():
     pass
 
 
-# Генератор выдает номера банковских карт в формате XXXX XXXX XXXX XXXX, где X — цифра номера карты.
-# Генератор может сгенерировать номера карт в заданном диапазоне от 0000 0000 0000 0001 до 9999 9999 9999 9999
-def card_number_generator():
-    pass
+def card_number_generator(start: int = 1, stop: int = 9999999999999999) -> Union[List[str], TypeError, ValueError]:
+    """Функция принимает целое число начало диапазона и конца,
+    выдает список номеров банковских карт в формате XXXX XXXX XXXX XXXX.
+    Неуказанный диапазон от 1 до 9999999999999999(16 цифр)"""
+
+    if not isinstance(start, int) or not isinstance(stop, int):
+        raise TypeError("Введено не числовое значение")
+
+    if start < 1 or stop > 9999999999999999:
+        raise ValueError("Введены числа не в диапазоне с 1 до 9999999999999999(16 цифр)")
+
+    numbers = []
+
+    for number in range(start, stop + 1):
+        format_number = str("{:16d}".format(number)).replace(" ", "0")
+        card_number_string = f"{format_number[:4]} {format_number[4:8]} {format_number[8:12]} {format_number[12:]}"
+        numbers.append(card_number_string)
+
+    return numbers
--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,8 +1,8 @@
+from typing import Any, Dict, List, Tuple
+
 import pytest
 
-from typing import List, Dict, Tuple, Callable
-
-from src.generators import filter_by_currency
+from src.generators import card_number_generator, filter_by_currency, transaction_descriptions
 
 
 def test_filter_by_currency(transactions: List[Dict], input_by_currency: Tuple[str, List[Dict]]) -> None:
@@ -19,7 +19,8 @@ def test_filter_by_currency_not_currency(transactions: List[Dict]) -> None:
 
 
 def test_filter_by_currency_not_transactions() -> None:
-    """Генератор не завершается ошибкой при обработке пустого списка или списка без соответствующих валютных операций"""
+    """Генератор не завершается ошибкой при обработке пустого списка или
+    списка без соответствующих валютных операций"""
     result = list(filter_by_currency([], "EUR"))
     assert result == []
 
@@ -30,8 +31,30 @@ def test_transaction_descriptions():
     pass
 
 
-# Напишите тесты, которые проверяют, что генератор выдает правильные номера карт в заданном диапазоне.
-# Проверьте корректность форматирования номеров карт.
-# Убедитесь, что генератор корректно обрабатывает крайние значения диапазона и правильно завершает генерацию.
-def test_card_number_generator():
-    pass
+@pytest.mark.parametrize(
+    "start_generator, stop_generator, expected",
+    [
+        (1, 3, ["0000 0000 0000 0001", "0000 0000 0000 0002", "0000 0000 0000 0003"]),
+        (10001, 10003, ["0000 0000 0001 0001", "0000 0000 0001 0002", "0000 0000 0001 0003"]),
+        (9999999999999998, 9999999999999999, ["9999 9999 9999 9998", "9999 9999 9999 9999"]),
+    ],
+)
+def test_card_number_generator(start_generator: int, stop_generator: int, expected: str) -> None:
+    """Тестирование, что функция правильно выдает номера карт в заданном диапазоне,
+    корректность форматирования номеров карт и корректно обрабатывает крайние значения"""
+    assert card_number_generator(start_generator, stop_generator) == expected
+
+
+@pytest.mark.parametrize("start_generator, stop_generator", [("1", 3), (1, "3")])
+def test_card_number_wrong_type(start_generator: Any, stop_generator: Any) -> None:
+    """Функция проверяет на корректные вводные данные"""
+    with pytest.raises(TypeError) as exc_info:
+        card_number_generator(start_generator, stop_generator)
+        assert str(exc_info.value) == "Введено не числовое значение"
+
+
+def test_card_number_not_correct_input() -> None:
+    """Функция проверяет на вводные данные не входящие в диапазон с 1 до 9999999999999999(16 цифр)"""
+    with pytest.raises(ValueError) as exc_info:
+        card_number_generator(0, 1)
+        assert str(exc_info.value) == "Введены числа не в диапазоне с 1 до 9999999999999999(16 цифр)"
