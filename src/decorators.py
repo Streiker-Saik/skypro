363e5f787ac900b8f563ea78101ce076d8f8ef68
Alexei <streiker.saik@gmail.com>
2025-02-12 16:10:46 +0300

    add decotarors and test_decorators

--- src/decorators.py
+++ src/decorators.py
@@ -1,4 +1,32 @@
-def log():
-    def decorator(func):
-        def inner(*args, **kwargs):
-            pass
+from functools import wraps
+from typing import Any, Callable, Optional
+
+
+def log(filename: Optional[str]) -> Callable:
+    def decorator(func: Callable) -> Callable:
+        @wraps(func)
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
+            try:
+                func(*args, **kwargs)
+                message = f"{func.__name__} ok."
+
+            except TypeError as error_message:
+                message = f"{func.__name__} error: TypeError: {error_message}. Inputs: {args}, {kwargs}."
+
+            except ValueError as error_message:
+                message = f"{func.__name__} error: ValueError: {error_message}. Inputs: {args}, {kwargs}."
+
+            except Exception as error_message:
+                message = f"{func.__name__} error: Exception: {error_message}. Inputs: {args}, {kwargs}."
+
+            if not filename:
+                print(message)
+            else:
+                with open(filename, "a", encoding="utf-8") as file:
+                    file.write(f"{message}\n")
+
+            # return result
+
+        return wrapper
+
+    return decorator
--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,2 +1,28 @@
-def test_log():
-    pass
+from typing import Any
+
+import pytest
+
+from src.decorators import log
+from src.masks import get_mask_card_number
+
+
+@pytest.mark.parametrize(
+    "argument, expected",
+    [
+        (7000792289606361, "get_mask_card_number ok.\n"),
+        (None, "get_mask_card_number error: TypeError: Вводные дынные отсутствуют. Inputs: (None,), {}.\n"),
+        ("", "get_mask_card_number error: TypeError: Введено не числовое значение. Inputs: ('',), {}.\n"),
+        (
+            70007922896063611,
+            "get_mask_card_number error: ValueError: В номере карты должно быть 16 цифр. Inputs: (70007922896063611,), {}.\n",
+        ),
+    ],
+)
+def test_log_get_mask_card_number(capsys: pytest.CaptureFixture, argument: Any, expected: str) -> None:
+    """Тестирование работы декоратора 'log', на разные данные функции 'get_mask_card_number'"""
+
+    decoder_get_mask_card_number = log(None)(get_mask_card_number)
+
+    decoder_get_mask_card_number(argument)
+    captured = capsys.readouterr()
+    assert (expected) in captured.out
