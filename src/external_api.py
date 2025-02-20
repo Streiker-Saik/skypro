64303c70ee92bd7fa6350b2005a018745fad5ff8
Alexei <streiker.saik@gmail.com>
2025-02-20 15:00:10 +0300

    add test_external_api.py

--- src/external_api.py
+++ src/external_api.py
@@ -1,18 +1,85 @@
 import json
 import os
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Optional
+import datetime
 
 import requests
 from dotenv import load_dotenv
 
 
-def transaction_amount_in_rub(transactions: List[Dict[str, Any]]) -> float:
-    """Функция, принимает транзакции, выводит сумму транзакций в рублях"""
-    code = "RUB"
-    transactions_amount = []
-    transactions_currency_amount = {}
+# def transaction_total_amount_in_rub(transactions: List[Dict[str, Any]]) -> float:
+#     """Функция, принимает транзакции, выводит сумму транзакций в рублях. С конвертацией на сегодняшний день"""
+#     code = "RUB"
+#     transactions_amount = []
+#     transactions_currency_amount = {}
+#
+#     for transaction in transactions:
+#         # {
+#         #     "id": 441945886,
+#         #     "state": "EXECUTED",
+#         #     "date": "2019-08-26T10:50:58.294041",
+#         #     "operationAmount": {
+#         #         "amount": "31957.58",
+#         #         "currency": {
+#         #             "name": "руб.",
+#         #             "code": "RUB"
+#         #         }
+#         #     },
+#         #     "description": "Перевод организации",
+#         #     "from": "Maestro 1596837868705199",
+#         #     "to": "Счет 64686473678894779589"
+#         # }
+#         code_currency = transaction["operationAmount"]["currency"]["code"]
+#         amount = float(transaction["operationAmount"]["amount"])
+#
+#         if code_currency not in transactions_currency_amount:
+#             transactions_currency_amount[code_currency] = amount
+#         else:
+#             transactions_currency_amount[code_currency] += amount
+#
+#     for key, values in transactions_currency_amount.items():
+#         if key == code:
+#             transactions_amount.append(round(values, 2))
+#         else:
+#             convert_rates = get_apilayer_convert_rates(code_to=code, code_from=key, amount=str(round(values, 2)))
+#             transactions_amount.append(convert_rates)
+#
+#     return sum(transactions_amount)
 
-    for transaction in transactions:
+
+def get_apilayer_convert_rates(date: Optional[str] = None, *, code_to: str, code_from: str, amount: str) -> float:
+    """Функция конвертации валюты, Exchange Rates Data API: https://apilayer.com/marketplace/exchangerates_data-api"""
+    load_dotenv("../.env")
+    api_key = os.getenv("APILAYER_EDAPI_KEY")
+
+    payload: Dict[Any, Any] = {}
+    headers = {"apikey": api_key}
+    url = f"https://api.apilayer.com/exchangerates_data/convert?to={code_to}&from={code_from}&amount={amount}"
+    if date:
+        url += f"&date={date}"
+
+    try:
+        response = requests.request("GET", url, headers=headers, data=payload)
+        status_code = response.status_code
+        result = response.text
+
+        if response.status_code != 200:
+            raise Exception(f"Ошибка API: {status_code} - {result}")
+
+        output_data = response.json()
+        return round(float(output_data["result"]), 2)
+
+    except requests.exceptions.ConnectionError:
+        raise Exception("Connection Error. Please check your network connection")
+
+    except Exception as exc_info:
+        raise Exception(f"Что-то пошло не так. {str(exc_info)}")
+
+
+def transaction_amount_in_rub(transaction: Dict[str, Any]) -> float:
+    """Функция, принимает транзакции, выводит сумму транзакции в рублях"""
+    code = "RUB"
+    try:
         # {
         #     "id": 441945886,
         #     "state": "EXECUTED",
@@ -29,52 +96,30 @@ def transaction_amount_in_rub(transactions: List[Dict[str, Any]]) -> float:
         #     "to": "Счет 64686473678894779589"
         # }
         code_currency = transaction["operationAmount"]["currency"]["code"]
-        amount = float(transaction["operationAmount"]["amount"])
-
-        if code_currency not in transactions_currency_amount:
-            transactions_currency_amount[code_currency] = amount
+        amount = transaction["operationAmount"]["amount"]
+        date_string = transaction["date"]
+        date_obj = datetime.datetime.strptime(date_string, "%Y-%m-%dT%H:%M:%S.%f")
+        # YYYY-MM-DD
+        date = date_obj.strftime("%Y-%m-%d")
+        if code_currency == "RUB":
+            result = round(float(amount), 2)
         else:
-            transactions_currency_amount[code_currency] += amount
+            result = get_apilayer_convert_rates(date, code_to=code, code_from=code_currency, amount=amount)
+        return result
 
-    for key, values in transactions_currency_amount.items():
-        if key == code:
-            transactions_amount.append(round(values, 2))
-        else:
-            convert_rates = get_apilayer_convert_rates(code_to=code, code_from=key, amount=str(round(values, 2)))
-            transactions_amount.append(convert_rates)
-
-    return sum(transactions_amount)
+    except KeyError as ext_info:
+        raise ValueError(f"Ключ не найден: {ext_info}")
 
 
-def get_apilayer_convert_rates(*, code_to: str, code_from: str, amount: str) -> float:
-    """Функция конвертации валюты, Exchange Rates Data API: https://apilayer.com/marketplace/exchangerates_data-api"""
-    load_dotenv("../.env")
-    apikey_ = os.getenv("APILAYER_EDAPI_KEY")
-
-    payload: Dict[Any, Any] = {}
-    headers = {"apikey": apikey_}
-    url = f"https://api.apilayer.com/exchangerates_data/convert?to={code_to}&from={code_from}&amount={amount}"
-
-    try:
-        response = requests.request("GET", url, headers=headers, data=payload)
-        status_code = response.status_code
-        result = response.text
-        output_data = json.loads(result)
-        return round(float(output_data["result"]), 2)
-        # return result.json()["result"]
-
-    except requests.exceptions.ConnectionError:
-        raise print("Connection Error. Please check your network connection")
-
-    except Exception:
-        raise print(f"Что-то пошло не так. Ошибка {status_code}")
-
-
-from src.utils import get_transactions_from_json
-if __name__ == "__main__":
-    # transactions = get_transactions_from_json("operations.json")
-    # print(type(transactions))
-    # result =transaction_amount_in_rub(transactions)
-    # print(type(result))
-    # print(result)
-    print(get_apilayer_convert_rates(code_to="RUB", code_from="USD", amount=1))
+# from src.utils import get_transactions_from_json
+# if __name__ == "__main__":
+#     print(get_apilayer_convert_rates("2000-01-01", code_to="RUB", code_from="USD", amount="1"))
+#     transactions = get_transactions_from_json("operations.json")
+#     count = 0
+#     for transaction in transactions:
+#         count += 1
+#         print(transaction_amount_in_rub(transaction))
+#         if count == 2:
+#             break
+#     result =transaction_total_amount_in_rub(transactions)
+#     print(result)
--- tests/test_external_api.py
+++ tests/test_external_api.py
@@ -1,29 +1,99 @@
-# import json
-# import os
-# from typing import Any, Dict, List
-#
-# import requests
-# from dotenv import load_dotenv
-# from unittest.mock import patch
-# from src.external_api import transaction_amount_in_rub, get_apilayer_convert_rates
-#
-#
-# def test_transaction_amount_in_rub():
-#     pass
-#
-#
-# @patch("requests.request")
-# def test_get_apilayer_convert_rates(mock_get):
-#     mock_get.return_value.json.return_value = {"result": 1}
-#     assert get_apilayer_convert_rates(code_to="RUB", code_from="USD", amount="1") == 1
-#     # load_dotenv("../.env")
-#     # apikey_ = os.getenv("APILAYER_EDAPI_KEY")
-#     # payload: Dict[Any, Any] = {}
-#     # headers = {"apikey": apikey_}
-#     url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from=USD&amount=1"
-#     requests.request("GET", url, headers=headers, data=payload)
-#
-#     # status_code = response.status_code
-#     # result = response.text
-#     # output_data = json.loads(result)
-#     # mock_get.assert_called_once_with(response)
+import os
+import pytest
+import requests
+from unittest.mock import patch
+from src.external_api import transaction_amount_in_rub, get_apilayer_convert_rates
+
+
+def test_transaction_amount_in_rub_rub() -> None:
+    """Тестирование при валюте в 'RUB'"""
+    transaction = {"date": "2019-08-26T10:50:58.294041", "operationAmount": {"amount": "31957.58", "currency": {"name": "руб.", "code": "RUB"}}}
+    assert transaction_amount_in_rub(transaction) == 31957.58
+
+
+def test_transaction_amount_in_rub_none_key() -> None:
+    """Тестирование если ключ в словаре отсутствует"""
+    with pytest.raises(ValueError) as exc_info:
+        transaction = {"operationAmount": {"amount": "31957.58", "currency": {"name": "руб.", }}}
+        transaction_amount_in_rub(transaction)
+        assert str(exc_info.value) == "Ключ не найден: 'code'"
+
+
+@patch("src.external_api.get_apilayer_convert_rates")
+def test_transaction_amount_in_rub_usd(mock_get) -> None:
+    """Тестирование при валюте в 'USD'"""
+    transaction = {"date": "2019-07-03T18:35:29.512364","operationAmount": {"amount": "8221.37", "currency": {"name": "USD", "code": "USD"}}}
+
+    mock_get.return_value = 520543.42
+    result = transaction_amount_in_rub(transaction)
+    assert result == 520543.42
+
+
+
+@patch("requests.request")
+def test_get_apilayer_convert_rates(mock_request):
+    """Тестирование, правильно ли функция возвращает при успешном запросе"""
+    code_to = "RUB"
+    code_from = "USD"
+    amount = "1"
+    expected_result = 88.95
+
+    mock_request.return_value.json.return_value = {"result": expected_result}
+    mock_request.return_value.status_code = 200
+
+    assert get_apilayer_convert_rates(code_to=code_to, code_from=code_from, amount=amount) == expected_result
+
+    api_key = os.getenv("APILAYER_EDAPI_KEY")
+    url = f"https://api.apilayer.com/exchangerates_data/convert?to={code_to}&from={code_from}&amount={amount}"
+    mock_request.assert_called_once_with("GET", url, headers={"apikey": api_key}, data={})
+
+
+@patch("requests.request")
+def test_get_apilayer_convert_rates_api_error(mock_request):
+    """Тестирование, правильно ли функция обрабатывает ошибки"""
+    code_to = "RUB"
+    code_from = "USD"
+    amount = "1"
+
+    mock_request.return_value.status_code = 429
+    mock_request.return_value.text = "You have"
+
+    with pytest.raises(Exception) as exc_info:
+        get_apilayer_convert_rates(code_to=code_to, code_from=code_from, amount=amount)
+
+    assert "Что-то пошло не так. Ошибка API: 429 - You have" in str(exc_info)
+
+
+@patch("requests.request")
+def test_get_apilayer_convert_rates_connection_error(mock_request):
+    """Тестирование, правильно ли функция обрабатывает ошибку соединения"""
+    code_to = "RUB"
+    code_from = "USD"
+    amount = "1"
+
+    # имитация ошибки соединения
+    mock_request.side_effect = requests.exceptions.ConnectionError
+
+    with pytest.raises(Exception) as exc_info:
+        get_apilayer_convert_rates(code_to=code_to, code_from=code_from, amount=amount)
+
+    assert "Connection Error. Please check your network connection" in str(exc_info)
+
+
+@patch("requests.request")
+def test_get_apilayer_convert_rates_introduction_date(mock_request):
+    """Тестирование, правильно ли функция возвращает при успешном запросе"""
+    code_to = "RUB"
+    code_from = "USD"
+    amount = "1"
+    date = "2000-01-01"
+    expected_result = 27.48
+
+    mock_request.return_value.json.return_value = {"result": expected_result}
+    mock_request.return_value.status_code = 200
+
+    assert get_apilayer_convert_rates(date, code_to=code_to, code_from=code_from, amount=amount) == expected_result
+
+    api_key = os.getenv("APILAYER_EDAPI_KEY")
+    url = f"https://api.apilayer.com/exchangerates_data/convert?to={code_to}&from={code_from}&amount={amount}&date={date}"
+    mock_request.assert_called_once_with("GET", url, headers={"apikey": api_key}, data={})
