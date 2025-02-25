aaa66ae1ac85935dab6a22199673b69ea20a2524
Alexei <streiker.saik@gmail.com>
2025-02-25 14:20:36 +0300

    начало работы

---
+++ src/transaction_reader.py
@@ -0,0 +1,10 @@
+import pandas
+from typing import Any, Dict, List
+
+
+def read_csv_transactions(file_path: str) -> List[Dict[str, Any]]:
+    pass
+
+
+def read_excel_transactions(file_path: str) -> List[Dict[str, Any]]:
+    pass
